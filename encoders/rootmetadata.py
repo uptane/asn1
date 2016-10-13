c7a4c365e4da0a842652253ebdd507e2f759ab26
Trishank Karthik Kuppusamy <trishank@nyu.edu>
2016-10-13 10:29:37 -0400

    Refactor code.

--- encoders/rootmetadata.py
+++ encoders/rootmetadata.py
@@ -12,138 +12,15 @@ def get_asn_signed(json_signed):
                  .subtype(implicitTag=tag.Tag(tag.tagClassContext,
                                               tag.tagFormatConstructed, 0))
 
-  keys = PublicKeys().subtype(implicitTag=tag.Tag(tag.tagClassContext,
-                                                  tag.tagFormatSimple, 1))
-
-  rootPublicKey = PublicKey()
-  # NOTE: Only 1 key allowed for now!
   rootPublicKeyid = json_signed['roles']['root']['keyids'][0]
-  rootPublicKey['publicKeyid'] = rootPublicKeyid
-  rootPublicKeyType = json_signed['keys'][rootPublicKeyid]['keytype']
-  rootPublicKey['publicKeyType'] = int(PublicKeyType(rootPublicKeyType))
-  rootPublicKeyValue = BinaryData()\
-                       .subtype(explicitTag=tag.Tag(tag.tagClassContext,
-                                                    tag.tagFormatConstructed,
-                                                    2))
-  rootPublicKeyHexString = json_signed['keys'][rootPublicKeyid]['keyval']\
-                                      ['public']
-  rootPublicKeyValue['hexString'] = rootPublicKeyHexString
-  rootPublicKey['publicKeyValue'] = rootPublicKeyValue
-  keys[0] = rootPublicKey
-
-  timestampPublicKey = PublicKey()
-  # NOTE: Only 1 key allowed for now!
   timestampPublicKeyid = json_signed['roles']['timestamp']['keyids'][0]
-  timestampPublicKey['publicKeyid'] = timestampPublicKeyid
-  timestampPublicKeyType = json_signed['keys'][timestampPublicKeyid]['keytype']
-  timestampPublicKey['publicKeyType'] = \
-                                      int(PublicKeyType(timestampPublicKeyType))
-  timestampPublicKeyValue = \
-                BinaryData().subtype(explicitTag=tag.Tag(tag.tagClassContext,
-                                                 tag.tagFormatConstructed, 2))
-  timestampPublicKeyHexString = json_signed['keys'][timestampPublicKeyid]\
-                                           ['keyval']['public']
-  timestampPublicKeyValue['hexString'] = timestampPublicKeyHexString
-  timestampPublicKey['publicKeyValue'] = timestampPublicKeyValue
-  keys[1] = timestampPublicKey
-
-  snapshotPublicKey = PublicKey()
-  # NOTE: Only 1 key allowed for now!
   snapshotPublicKeyid = json_signed['roles']['snapshot']['keyids'][0]
-  snapshotPublicKey['publicKeyid'] = snapshotPublicKeyid
-  snapshotPublicKeyType = json_signed['keys'][snapshotPublicKeyid]['keytype']
-  snapshotPublicKey['publicKeyType'] = \
-                                      int(PublicKeyType(snapshotPublicKeyType))
-  snapshotPublicKeyValue = \
-          BinaryData().subtype(explicitTag=tag.Tag(tag.tagClassContext,
-                                                   tag.tagFormatConstructed, 2))
-  snapshotPublicKeyHexString = json_signed['keys'][snapshotPublicKeyid]\
-                                          ['keyval']['public']
-  snapshotPublicKeyValue['hexString'] = snapshotPublicKeyHexString
-  snapshotPublicKey['publicKeyValue'] = snapshotPublicKeyValue
-  keys[2] = snapshotPublicKey
-
-  targetsPublicKey = PublicKey()
-  # NOTE: Only 1 key allowed for now!
   targetsPublicKeyid = json_signed['roles']['targets']['keyids'][0]
-  targetsPublicKey['publicKeyid'] = targetsPublicKeyid
-  targetsPublicKeyType = json_signed['keys'][targetsPublicKeyid]['keytype']
-  targetsPublicKey['publicKeyType'] = \
-                                      int(PublicKeyType(targetsPublicKeyType))
-  targetsPublicKeyValue = BinaryData()\
-                          .subtype(explicitTag=tag.Tag(tag.tagClassContext,
-                                                       tag.tagFormatConstructed,
-                                                       2))
-  targetsPublicKeyHexString = json_signed['keys'][targetsPublicKeyid]\
-                                          ['keyval']['public']
-  targetsPublicKeyValue['hexString'] = targetsPublicKeyHexString
-  targetsPublicKey['publicKeyValue'] = targetsPublicKeyValue
-  keys[3] = targetsPublicKey
 
-  rootMetadata['numberOfKeys'] = 4
-  rootMetadata['keys'] = keys
-
-  roles = TopLevelRoles().subtype(implicitTag=tag.Tag(tag.tagClassContext,
-                                                      tag.tagFormatConstructed,
-                                                      3))
-
-  rootRole = TopLevelRole()
-  rootRole['role'] = int(RoleType('root'))
-  rootRoleKeyids = Keyids().subtype(implicitTag=tag.Tag(tag.tagClassContext,
-                                                        tag.tagFormatSimple, 4))
-  rootRoleKeyid = Keyid(rootPublicKeyid)
-  # Some damned bug in pyasn1 I could not care less to fix right now.
-  rootRoleKeyids.setComponentByPosition(0, rootRoleKeyid, False)
-  rootRole['numberOfKeyids'] = 1
-  rootRole['keyids'] = rootRoleKeyids
-  rootRole['threshold'] = 1
-  # Some damned bug in pyasn1 I could not care less to fix right now.
-  roles.setComponentByPosition(0, rootRole, False)
-
-  snapshotRole = TopLevelRole()
-  snapshotRole['role'] = int(RoleType('snapshot'))
-  snapshotRoleKeyids = Keyids().subtype(implicitTag=tag.Tag(tag.tagClassContext,
-                                                            tag.tagFormatSimple,
-                                                            4))
-  snapshotRoleKeyid = Keyid(snapshotPublicKeyid)
-  # Some damned bug in pyasn1 I could not care less to fix right now.
-  snapshotRoleKeyids.setComponentByPosition(0, snapshotRoleKeyid, False)
-  snapshotRole['numberOfKeyids'] = 1
-  snapshotRole['keyids'] = snapshotRoleKeyids
-  snapshotRole['threshold'] = 1
-  # Some damned bug in pyasn1 I could not care less to fix right now.
-  roles.setComponentByPosition(1, snapshotRole, False)
-
-  targetsRole = TopLevelRole()
-  targetsRole['role'] = int(RoleType('targets'))
-  targetsRoleKeyids = Keyids().subtype(implicitTag=tag.Tag(tag.tagClassContext,
-                                                           tag.tagFormatSimple,
-                                                           4))
-  targetsRoleKeyid = Keyid(targetsPublicKeyid)
-  # Some damned bug in pyasn1 I could not care less to fix right now.
-  targetsRoleKeyids.setComponentByPosition(0, targetsRoleKeyid, False)
-  targetsRole['numberOfKeyids'] = 1
-  targetsRole['keyids'] = targetsRoleKeyids
-  targetsRole['threshold'] = 1
-  # Some damned bug in pyasn1 I could not care less to fix right now.
-  roles.setComponentByPosition(2, targetsRole, False)
-
-  timestampRole = TopLevelRole()
-  timestampRole['role'] = int(RoleType('timestamp'))
-  timestampRoleKeyids = Keyids()\
-                        .subtype(implicitTag=tag.Tag(tag.tagClassContext,
-                                                     tag.tagFormatSimple, 4))
-  timestampRoleKeyid = Keyid(timestampPublicKeyid)
-  # Some damned bug in pyasn1 I could not care less to fix right now.
-  timestampRoleKeyids.setComponentByPosition(0, timestampRoleKeyid, False)
-  timestampRole['numberOfKeyids'] = 1
-  timestampRole['keyids'] = timestampRoleKeyids
-  timestampRole['threshold'] = 1
-  # Some damned bug in pyasn1 I could not care less to fix right now.
-  roles.setComponentByPosition(3, timestampRole, False)
-
-  rootMetadata['numberOfRoles'] = 4
-  rootMetadata['roles'] = roles
+  keys = set_keys(json_signed, rootPublicKeyid, timestampPublicKeyid,
+                  snapshotPublicKeyid, targetsPublicKeyid, rootMetadata)
+  roles = set_roles(json_signed, rootPublicKeyid, timestampPublicKeyid,
+                  snapshotPublicKeyid, targetsPublicKeyid, rootMetadata)
 
   signedBody = SignedBody()\
                .subtype(explicitTag=tag.Tag(tag.tagClassContext,
@@ -217,6 +94,141 @@ def get_json_signed(asn_metadata):
   json_signed['roles'] = json_roles
 
   return json_signed
+
+
+def set_keys(json_signed, rootPublicKeyid, timestampPublicKeyid,
+             snapshotPublicKeyid, targetsPublicKeyid, rootMetadata):
+  keys = PublicKeys().subtype(implicitTag=tag.Tag(tag.tagClassContext,
+                                                  tag.tagFormatSimple, 1))
+
+  rootPublicKey = PublicKey()
+  # NOTE: Only 1 key allowed for now!
+  rootPublicKey['publicKeyid'] = rootPublicKeyid
+  rootPublicKeyType = json_signed['keys'][rootPublicKeyid]['keytype']
+  rootPublicKey['publicKeyType'] = int(PublicKeyType(rootPublicKeyType))
+  rootPublicKeyValue = BinaryData()\
+                       .subtype(explicitTag=tag.Tag(tag.tagClassContext,
+                                                    tag.tagFormatConstructed,
+                                                    2))
+  rootPublicKeyHexString = json_signed['keys'][rootPublicKeyid]['keyval']\
+                                      ['public']
+  rootPublicKeyValue['hexString'] = rootPublicKeyHexString
+  rootPublicKey['publicKeyValue'] = rootPublicKeyValue
+  keys[0] = rootPublicKey
+
+  timestampPublicKey = PublicKey()
+  # NOTE: Only 1 key allowed for now!
+  timestampPublicKey['publicKeyid'] = timestampPublicKeyid
+  timestampPublicKeyType = json_signed['keys'][timestampPublicKeyid]['keytype']
+  timestampPublicKey['publicKeyType'] = \
+                                      int(PublicKeyType(timestampPublicKeyType))
+  timestampPublicKeyValue = \
+                BinaryData().subtype(explicitTag=tag.Tag(tag.tagClassContext,
+                                                 tag.tagFormatConstructed, 2))
+  timestampPublicKeyHexString = json_signed['keys'][timestampPublicKeyid]\
+                                           ['keyval']['public']
+  timestampPublicKeyValue['hexString'] = timestampPublicKeyHexString
+  timestampPublicKey['publicKeyValue'] = timestampPublicKeyValue
+  keys[1] = timestampPublicKey
+
+  snapshotPublicKey = PublicKey()
+  # NOTE: Only 1 key allowed for now!
+  snapshotPublicKey['publicKeyid'] = snapshotPublicKeyid
+  snapshotPublicKeyType = json_signed['keys'][snapshotPublicKeyid]['keytype']
+  snapshotPublicKey['publicKeyType'] = \
+                                      int(PublicKeyType(snapshotPublicKeyType))
+  snapshotPublicKeyValue = \
+          BinaryData().subtype(explicitTag=tag.Tag(tag.tagClassContext,
+                                                   tag.tagFormatConstructed, 2))
+  snapshotPublicKeyHexString = json_signed['keys'][snapshotPublicKeyid]\
+                                          ['keyval']['public']
+  snapshotPublicKeyValue['hexString'] = snapshotPublicKeyHexString
+  snapshotPublicKey['publicKeyValue'] = snapshotPublicKeyValue
+  keys[2] = snapshotPublicKey
+
+  targetsPublicKey = PublicKey()
+  # NOTE: Only 1 key allowed for now!
+  targetsPublicKey['publicKeyid'] = targetsPublicKeyid
+  targetsPublicKeyType = json_signed['keys'][targetsPublicKeyid]['keytype']
+  targetsPublicKey['publicKeyType'] = \
+                                      int(PublicKeyType(targetsPublicKeyType))
+  targetsPublicKeyValue = BinaryData()\
+                          .subtype(explicitTag=tag.Tag(tag.tagClassContext,
+                                                       tag.tagFormatConstructed,
+                                                       2))
+  targetsPublicKeyHexString = json_signed['keys'][targetsPublicKeyid]\
+                                          ['keyval']['public']
+  targetsPublicKeyValue['hexString'] = targetsPublicKeyHexString
+  targetsPublicKey['publicKeyValue'] = targetsPublicKeyValue
+  keys[3] = targetsPublicKey
+
+  rootMetadata['numberOfKeys'] = 4
+  rootMetadata['keys'] = keys
+
+
+def set_roles(json_signed, rootPublicKeyid, timestampPublicKeyid,
+              snapshotPublicKeyid, targetsPublicKeyid, rootMetadata):
+  roles = TopLevelRoles().subtype(implicitTag=tag.Tag(tag.tagClassContext,
+                                                      tag.tagFormatConstructed,
+                                                      3))
+
+  rootRole = TopLevelRole()
+  rootRole['role'] = int(RoleType('root'))
+  rootRoleKeyids = Keyids().subtype(implicitTag=tag.Tag(tag.tagClassContext,
+                                                        tag.tagFormatSimple, 4))
+  rootRoleKeyid = Keyid(rootPublicKeyid)
+  # Some damned bug in pyasn1 I could not care less to fix right now.
+  rootRoleKeyids.setComponentByPosition(0, rootRoleKeyid, False)
+  rootRole['numberOfKeyids'] = 1
+  rootRole['keyids'] = rootRoleKeyids
+  rootRole['threshold'] = 1
+  # Some damned bug in pyasn1 I could not care less to fix right now.
+  roles.setComponentByPosition(0, rootRole, False)
+
+  snapshotRole = TopLevelRole()
+  snapshotRole['role'] = int(RoleType('snapshot'))
+  snapshotRoleKeyids = Keyids().subtype(implicitTag=tag.Tag(tag.tagClassContext,
+                                                            tag.tagFormatSimple,
+                                                            4))
+  snapshotRoleKeyid = Keyid(snapshotPublicKeyid)
+  # Some damned bug in pyasn1 I could not care less to fix right now.
+  snapshotRoleKeyids.setComponentByPosition(0, snapshotRoleKeyid, False)
+  snapshotRole['numberOfKeyids'] = 1
+  snapshotRole['keyids'] = snapshotRoleKeyids
+  snapshotRole['threshold'] = 1
+  # Some damned bug in pyasn1 I could not care less to fix right now.
+  roles.setComponentByPosition(1, snapshotRole, False)
+
+  targetsRole = TopLevelRole()
+  targetsRole['role'] = int(RoleType('targets'))
+  targetsRoleKeyids = Keyids().subtype(implicitTag=tag.Tag(tag.tagClassContext,
+                                                           tag.tagFormatSimple,
+                                                           4))
+  targetsRoleKeyid = Keyid(targetsPublicKeyid)
+  # Some damned bug in pyasn1 I could not care less to fix right now.
+  targetsRoleKeyids.setComponentByPosition(0, targetsRoleKeyid, False)
+  targetsRole['numberOfKeyids'] = 1
+  targetsRole['keyids'] = targetsRoleKeyids
+  targetsRole['threshold'] = 1
+  # Some damned bug in pyasn1 I could not care less to fix right now.
+  roles.setComponentByPosition(2, targetsRole, False)
+
+  timestampRole = TopLevelRole()
+  timestampRole['role'] = int(RoleType('timestamp'))
+  timestampRoleKeyids = Keyids()\
+                        .subtype(implicitTag=tag.Tag(tag.tagClassContext,
+                                                     tag.tagFormatSimple, 4))
+  timestampRoleKeyid = Keyid(timestampPublicKeyid)
+  # Some damned bug in pyasn1 I could not care less to fix right now.
+  timestampRoleKeyids.setComponentByPosition(0, timestampRoleKeyid, False)
+  timestampRole['numberOfKeyids'] = 1
+  timestampRole['keyids'] = timestampRoleKeyids
+  timestampRole['threshold'] = 1
+  # Some damned bug in pyasn1 I could not care less to fix right now.
+  roles.setComponentByPosition(3, timestampRole, False)
+
+  rootMetadata['numberOfRoles'] = 4
+  rootMetadata['roles'] = roles
 
 
 if __name__ == '__main__':
